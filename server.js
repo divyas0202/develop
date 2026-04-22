7d5bd175f4a8258f363c9ae2eb7a62563b66817f
divyas0202 <divya.s2023ece@sece.ac.in>
2026-04-23 00:14:35 +0530

    fix cors

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
+app.use(cors({
+  origin: "*"
+}));
 require("dotenv").config();
 
 const app = express();
