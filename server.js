ca8723d83712adb8f552938b09aa02f7ddc761e4
divyas0202 <divya.s2023ece@sece.ac.in>
2026-04-23 00:24:20 +0530

    fix cors

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-app.use(cors({
-  origin: "*"
-}));
+
 require("dotenv").config();
 
 const app = express();
@@ -13,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
+app.use(cors({
+  origin: "*"
+}));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
